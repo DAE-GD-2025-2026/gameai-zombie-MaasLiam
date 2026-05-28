73dbc5bf21dd4f6dbcea30a2d13512fb469f1961
MaasLiam <liammaa2002@gmail.com>
2026-05-28 15:36:14 +0200

    Added debug logging for preception types

--- Plugins/GameFeatures/MaasLiamZombie/Source/MaasLiamZombieRuntime/StudentPerceptor.cpp
+++ Plugins/GameFeatures/MaasLiamZombie/Source/MaasLiamZombieRuntime/StudentPerceptor.cpp
@@ -21,6 +21,52 @@ void UStudentPerceptor::BeginPlay()
 
 void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
-	FString::Printf(TEXT("Saw Something!")));
+	if (!Actor) return;
+
+	FString ActorName = Actor->GetName();
+	FColor DebugColor = FColor::White;
+	FString TypeString = "Unknown";
+
+	//Zombies
+	if (ActorName.Contains("Zombie"))
+	{
+		TypeString = "Zombie";
+		DebugColor = FColor::Red;
+	}
+	//Items
+	else if (
+		ActorName.Contains("Food") ||
+		ActorName.Contains("Medkit") ||
+		ActorName.Contains("Pistol") ||
+		ActorName.Contains("Shotgun") ||
+		ActorName.Contains("Garbage"))
+	{
+		TypeString = "Item";
+		DebugColor = FColor::Green;
+	}
+	//Houses
+	else if (ActorName.Contains("House"))
+	{
+		TypeString = "House";
+		DebugColor = FColor::Blue;
+	}
+	//Purge zones
+	else if (ActorName.Contains("Purge"))
+	{
+		TypeString = "PurgeZone";
+		DebugColor = FColor::Purple;
+	}
+
+	FString DebugMessage = FString::Printf(
+		TEXT("Detected %s : %s"),
+		*TypeString,
+		*ActorName
+	);
+
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		2.f,
+		DebugColor,
+		DebugMessage
+	);
 }
