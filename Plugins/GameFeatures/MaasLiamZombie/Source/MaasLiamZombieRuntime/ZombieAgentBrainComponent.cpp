c4788baa15c6c84a5a516695f7982096b14b7df6
MaasLiam <liammaa2002@gmail.com>
2026-05-28 16:45:36 +0200

    Add injected agent brain component

---
+++ Plugins/GameFeatures/MaasLiamZombie/Source/MaasLiamZombieRuntime/ZombieAgentBrainComponent.cpp
@@ -0,0 +1,52 @@
+﻿#include "ZombieAgentBrainComponent.h"
+
+UZombieAgentBrainComponent::UZombieAgentBrainComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UZombieAgentBrainComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UZombieAgentBrainComponent::TickComponent(
+	float DeltaTime,
+	ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UpdateState();
+
+	GEngine->AddOnScreenDebugMessage(
+		20,
+		0.f,
+		FColor::Cyan,
+		FString::Printf(TEXT("AI State: %s"), *GetStateName())
+	);
+}
+
+void UZombieAgentBrainComponent::UpdateState()
+{
+	CurrentState = EZombieAgentState::Explore;
+}
+
+FString UZombieAgentBrainComponent::GetStateName() const
+{
+	switch (CurrentState)
+	{
+	case EZombieAgentState::Explore:
+		return "Explore";
+	case EZombieAgentState::SeekItem:
+		return "SeekItem";
+	case EZombieAgentState::Flee:
+		return "Flee";
+	case EZombieAgentState::Fight:
+		return "Fight";
+	case EZombieAgentState::UseItem:
+		return "UseItem";
+	default:
+		return "Unknown";
+	}
+}
---
+++ Plugins/GameFeatures/MaasLiamZombie/Source/MaasLiamZombieRuntime/ZombieAgentBrainComponent.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ZombieAgentBrainComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EZombieAgentState : uint8
+{
+	Explore UMETA(DisplayName = "Explore"),
+	SeekItem UMETA(DisplayName = "Seek Item"),
+	Flee UMETA(DisplayName = "Flee"),
+	Fight UMETA(DisplayName = "Fight"),
+	UseItem UMETA(DisplayName = "Use Item")
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MAASLIAMZOMBIERUNTIME_API UZombieAgentBrainComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UZombieAgentBrainComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	EZombieAgentState CurrentState = EZombieAgentState::Explore;
+
+	void UpdateState();
+	FString GetStateName() const;
+};
